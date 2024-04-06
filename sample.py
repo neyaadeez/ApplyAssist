ae6e9e407830651fab9032f6660a0eab5f29b9a4
Mustafa Mokashi <neyaadeez@gmail.com>
2024-04-05 21:31:50 -0700

    test

--- sample.py
+++ sample.py
@@ -1,5 +1,22 @@
+# Python program to demonstrate
+# selenium
+
+# import webdriver
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# create webdriver object
 driver = webdriver.Chrome()
-driver.get("https://github.com/neyaadeez")
-script = "alert('Alert via selenium')"
-driver.execute_async_script(script) 
+
+# enter keyword to search
+keyword = "geeksforgeeks"
+
+# get geeksforgeeks.org
+driver.get("https://www.geeksforgeeks.org/")
+
+login_form = driver.find_element(By.XPATH, "//form[1]")
+login_form.send_keys("hello world")
+login_form.click()
+
+# print complete element
+print(login_form)
